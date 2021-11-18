6e7fa4fed993889aca5f32bd3641e0c77e2e3c2a
messica <wuyanbin888888@163.com>
2021-11-18 19:33:55 +0800

    feat: 配置代码格式化工具

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// Documentation for this file: https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  requirePragma: false,
+  HTMLWhitespaceSensitivity: "ignore"
+};
